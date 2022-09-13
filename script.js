9593b13b879c33ca761561878ab52faf5c8fd511
Migue C <miguecisterna@gmail.com>
2022-09-13 17:40:56 -0300

    Agregadas palabras y espacios , tambien event listener para escuchar teclado

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let listaDePalabras = ["Abaco","Berenjena","Calido","Dios","Esfinge","Femur","Gelido","Hipocampo","Iglu","Joroba","Kiosco","Lagrima","Moneda","Notario","Orificio","Pedazo","Queso","Rio","Saturno","Tiburon","Ultimo","Vasos","Washington","Xilofon","Yendo","Zamba"];
+let palabraSecreta = "";
+let teclaPulsada = "";
+
+
+
 //Funciones:
 function iniciarJuego(){
     
@@ -5,6 +11,25 @@ function iniciarJuego(){
     document.getElementById("body").style.backgroundColor = "#8FDE83";
     document.getElementById("logoAlura").style.backgroundImage = "url('images/aluralightgreen.png')";
     document.getElementById("containerJuego").style.display = "block";
+    elegirPalabraSecreta();
+    console.log(palabraSecreta);
+    
+    for(let i = 0; i < palabraSecreta.length; i++){
+        var tag = document.createElement("p");
+        var tag2 = document.createElement("p");
+        var letra = document.createTextNode(palabraSecreta[i]);
+        var espacio = document.createTextNode("¯");
+
+        tag.appendChild(letra);
+        tag.classList.add("claseLetra");
+
+        tag2.appendChild(espacio);       
+
+        document.getElementById("letras").appendChild(tag);
+        document.getElementById("espacios").appendChild(tag2);
+
+        var letraSecreta = document.getElementById("letras").appendChild(tag);        
+    }
 }
 
 function terminarJuego(){
@@ -13,4 +38,18 @@ function terminarJuego(){
     document.getElementById("body").style.backgroundColor = "#C7F59D";
     document.getElementById("logoAlura").style.backgroundImage = "url('images/aluragreen.png')";
     document.getElementById("containerJuego").style.display = "none";
+    palabraSecreta = "";
+    document.getElementById("letras").innerHTML = "";
+    document.getElementById("espacios").innerHTML = "";
 }
+
+function elegirPalabraSecreta(){
+    palabraSecreta = listaDePalabras[Math.floor(Math.random() * listaDePalabras.length)];
+}
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    teclaPulsada = key;
+    key.length === 1;
+    console.log(teclaPulsada);
+});
